0c5ce49240f5df43934e38b8ef277cd3544085ab
unknown <chloecassanjouin@gmail.com>
2024-09-12 15:28:32 +0200

    vider formulaire au clic

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -6,6 +6,84 @@ export const FIELD_TYPES = {
   TEXTAREA: 2,
 };
 
+const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder, className, value, onChange }) => {
+  let component;
+  switch (type) {
+    case FIELD_TYPES.INPUT_TEXT:
+      component = (
+        <input
+          type="text"
+          name={name}
+          placeholder={placeholder}
+          className={className}
+          value={value} // Ajoutez la valeur ici
+          onChange={onChange} // Ajoutez le gestionnaire d'événements ici
+          data-testid="field-testid"
+        />
+      );
+      break;
+    case FIELD_TYPES.TEXTAREA:
+      component = (
+        <textarea
+          name={name}
+          className={className}
+          value={value} // Ajoutez la valeur ici
+          onChange={onChange} // Ajoutez le gestionnaire d'événements ici
+          data-testid="field-testid"
+        />
+      );
+      break;
+    default:
+      component = (
+        <input
+          type="text"
+          name={name}
+          placeholder={placeholder}
+          className={className}
+          value={value} // Ajoutez la valeur ici
+          onChange={onChange} // Ajoutez le gestionnaire d'événements ici
+          data-testid="field-testid"
+        />
+      );
+  }
+  return (
+    <div className="inputField">
+      <span>{label}</span>
+      {component}
+    </div>
+  );
+};
+
+Field.propTypes = {
+  type: PropTypes.oneOf(Object.values(FIELD_TYPES)),
+  name: PropTypes.string,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  className: PropTypes.string,
+  value: PropTypes.string, // Ajoutez la déclaration de type ici
+  onChange: PropTypes.func, // Ajoutez la déclaration de type ici
+};
+
+Field.defaultProps = {
+  label: "",
+  placeholder: "",
+  type: FIELD_TYPES.INPUT_TEXT,
+  name: "field-name",
+  className: "",
+  value: "", // Valeur par défaut vide
+  onChange: () => {}, // Valeur par défaut fonction vide
+};
+
+export default Field;
+
+/* import PropTypes from "prop-types";
+import "./style.scss";
+
+export const FIELD_TYPES = {
+  INPUT_TEXT: 1,
+  TEXTAREA: 2,
+};
+
 const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder, className }) => {
   let component;
   switch (type) {
@@ -64,4 +142,4 @@ Field.defaultProps = {
   className: "",  // Valeur par défaut vide
 };
 
-export default Field;
+export default Field; */
--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -4,6 +4,125 @@ import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
+const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 500); });
+
+const Form = ({ onSuccess, onError }) => {
+  const [sending, setSending] = useState(false);
+
+  // État pour les valeurs du formulaire
+  const [formData, setFormData] = useState({
+    nom: '',
+    prenom: '',
+    type: '',
+    email: '',
+    message: ''
+  });
+
+  // Fonction pour mettre à jour les champs
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  // Fonction pour gérer l'envoi du formulaire
+  const sendContact = useCallback(
+    async (evt) => {
+      evt.preventDefault();
+      setSending(true);
+      try {
+        await mockContactApi();
+        setSending(false);
+        setFormData({
+          nom: '',
+          prenom: '',
+          type: '',
+          email: '',
+          message: ''
+        });
+        onSuccess(); // rajout Chloé pour affichage modale
+      } catch (err) {
+        setSending(false);
+        onError(err);
+      }
+    },
+    [onSuccess, onError]
+  );
+
+  return (
+    <form onSubmit={sendContact}>
+      <div className="row">
+        <div className="col">
+          <Field
+            name="nom"
+            placeholder=""
+            label="Nom"
+            value={formData.nom}
+            onChange={handleChange}
+          />
+          <Field
+            name="prenom"
+            placeholder=""
+            label="Prénom"
+            value={formData.prenom}
+            onChange={handleChange}
+          />
+          <Select
+            name="type"
+            selection={["Personel", "Entreprise"]}
+            onChange={handleChange} // Assurez-vous que le gestionnaire de changement est approprié
+            label="Personel / Entreprise"
+            type="large"
+            titleEmpty
+            value={formData.type} // Passez la valeur actuelle
+          />
+          <Field
+            name="email"
+            className="email-field"
+            placeholder=""
+            label="Email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
+            {sending ? "En cours" : "Envoyer"}
+          </Button>
+        </div>
+        <div className="col">
+          <Field
+            name="message"
+            placeholder="message"
+            label="Message"
+            type={FIELD_TYPES.TEXTAREA}
+            value={formData.message}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+    </form>
+  );
+};
+
+Form.propTypes = {
+  onError: PropTypes.func,
+  onSuccess: PropTypes.func,
+};
+
+Form.defaultProps = {
+  onError: () => null,
+  onSuccess: () => null,
+};
+
+export default Form;
+
+/* import { useCallback, useState } from "react";
+import PropTypes from "prop-types";
+import Field, { FIELD_TYPES } from "../../components/Field";
+import Select from "../../components/Select";
+import Button, { BUTTON_TYPES } from "../../components/Button";
+
 const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 500); })
 
 const Form = ({ onSuccess, onError }) => {
@@ -64,4 +183,4 @@ Form.defaultProps = {
   onSuccess: () => null,
 }
 
-export default Form;
+export default Form; */
