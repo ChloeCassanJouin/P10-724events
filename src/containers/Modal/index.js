b2467effdab5e5fb8b91772faff6e565f9466b7e
unknown <chloecassanjouin@gmail.com>
2024-09-24 13:22:41 +0200

    fermeture modale au clic extérieur

--- src/containers/Modal/index.js
+++ src/containers/Modal/index.js
@@ -1,16 +1,36 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Icon from "../../components/Icon";
 import "./style.scss";
 
 const Modal = ({ opened, Content, children }) => {
   const [isOpened, setIsOpened] = useState(opened);
+  const modalRef = useRef(null); // Référence pour le conteneur de la modale
+
+  const handleClickOutside = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      setIsOpened(false); // Ferme la modale si on clique à l'extérieur
+    }
+  };
+
+  useEffect(() => {
+    if (isOpened) {
+      document.addEventListener("mousedown", handleClickOutside); // Écouteur pour les clics
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside); // Nettoie l'écouteur
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside); // Nettoyage de l'écouteur lors de la destruction du composant
+    };
+  }, [isOpened]);
+
   return (
     <>
       {children({ isOpened, setIsOpened })}
       {isOpened && (
         <div className="modal">
-          <div className="content">
+          <div className="content" ref={modalRef}>
             {Content}
             <button
               type="button"
